328532f0a25163da67001c4d17227670406b8079
p72b <willy.riechert@gmail.com>
2020-02-14 10:36:08 +0100

    Add historical share graph and adjusted style.

--- app/src/main/java/de/p72b/burstpooltracker/main/statistic/StatisticFragment.kt
+++ app/src/main/java/de/p72b/burstpooltracker/main/statistic/StatisticFragment.kt
@@ -18,10 +18,21 @@ import de.p72b.burstpooltracker.Utils
 
 class StatisticFragment : Fragment() {
 
+    private var gradient: IntArray =
+        intArrayOf(Color.parseColor("#364d5a"), Color.parseColor("#3f7178"))
+    private var color: Int = Color.parseColor("#53c1bd")
+    private var dotsColor: Int = Color.parseColor("#758cbb")
+    private var lineThickness = 0.8f
+    private var fillColor: Int = Color.parseColor("#3d6c73")
     private lateinit var lineChart: LineChartView
     private lateinit var lineChartPlotSize: LineChartView
+    private lateinit var lineChartHistoricalShare: LineChartView
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.fragment_statistics, null)
     }
 
@@ -30,6 +41,7 @@ class StatisticFragment : Fragment() {
 
         lineChart = view.findViewById(R.id.vLineChartBurst)
         lineChartPlotSize = view.findViewById(R.id.vLineChartBurstPlotSize)
+        lineChartHistoricalShare = view.findViewById(R.id.vLineChartHistoricalShare)
 
         (activity as MainActivity).minerViewModel.allMiners.observe(this,
             Observer<List<Miner>> { miners ->
@@ -40,14 +52,16 @@ class StatisticFragment : Fragment() {
     }
 
     private fun setMiners(miners: List<Miner>) {
-        fillBurst(miners)
-        fillPlotSize(miners)
-    }
-
-    private fun fillBurst(miners: List<Miner>) {
         val size = miners.size
         val lables = arrayOfNulls<String>(size)
         val values = FloatArray(size)
+
+        fillBurst(miners, lables, values)
+        fillPlotSize(miners, lables, values)
+        fillHistoricalShare(miners, lables, values)
+    }
+
+    private fun fillBurst(miners: List<Miner>, lables: Array<String?>, values: FloatArray) {
 
         for ((index, value) in miners.withIndex()) {
             lables[index] = " " //Utils.timeStampToIsoDate(value.timeMilliseconds)
@@ -55,36 +69,51 @@ class StatisticFragment : Fragment() {
         }
 
         val dataSet = LineSet(lables, values)
-        dataSet.setColor(Color.parseColor("#53c1bd")).setFill(Color.parseColor("#3d6c73"))
-            .setGradientFill(intArrayOf(Color.parseColor("#364d5a"), Color.parseColor("#3f7178")), null)
-
-        //dataSet.setColor(Color.parseColor("#758cbb"))
-        //    .setFill(Color.parseColor("#2d374c"))
-        //    .setDotsColor(Color.parseColor("#758cbb")).thickness = 1.0f
+        dataSet.setColor(color)
+            .setFill(fillColor)
+            .setGradientFill(gradient, null)
+            //.setDotsColor(dotsColor)
+            .thickness = lineThickness
         lineChart.addData(dataSet as ChartSet)
         lineChart.dismissAllTooltips()
         lineChart.show()
     }
 
-    private fun fillPlotSize(miners: List<Miner>) {
-        val size = miners.size
-        val lables = arrayOfNulls<String>(size)
-        val values = FloatArray(size)
-
+    private fun fillPlotSize(miners: List<Miner>, lables: Array<String?>, values: FloatArray) {
         for ((index, value) in miners.withIndex()) {
             lables[index] = " " //Utils.timeStampToIsoDate(value.timeMilliseconds)
             values[index] = value.plotSize.toFloat()
         }
 
         val dataSet = LineSet(lables, values)
-        dataSet.setColor(Color.parseColor("#53c1bd")).setFill(Color.parseColor("#3d6c73"))
-            .setGradientFill(intArrayOf(Color.parseColor("#364d5a"), Color.parseColor("#3f7178")), null)
-
-        //dataSet.setColor(Color.parseColor("#758cbb"))
-        //    .setFill(Color.parseColor("#2d374c"))
-        //    .setDotsColor(Color.parseColor("#758cbb")).thickness = 1.0f
+        dataSet.setColor(color)
+            .setFill(fillColor)
+            .setGradientFill(gradient, null)
+            //.setDotsColor(dotsColor)
+            .thickness = lineThickness
         lineChartPlotSize.addData(dataSet as ChartSet)
         lineChartPlotSize.dismissAllTooltips()
         lineChartPlotSize.show()
+    }
+
+    private fun fillHistoricalShare(
+        miners: List<Miner>,
+        lables: Array<String?>,
+        values: FloatArray
+    ) {
+        for ((index, value) in miners.withIndex()) {
+            lables[index] = " "
+            values[index] = value.historicalShare.toFloat()
+        }
+
+        val dataSet = LineSet(lables, values)
+        dataSet.setColor(color)
+            .setFill(fillColor)
+            .setGradientFill(gradient, null)
+            //.setDotsColor(dotsColor)
+            .thickness = lineThickness
+        lineChartHistoricalShare.addData(dataSet as ChartSet)
+        lineChartHistoricalShare.dismissAllTooltips()
+        lineChartHistoricalShare.show()
     }
 }
